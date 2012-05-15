f869044fb775b10edcd2a8f1548aec56b07a1994
Taylor Otwell <taylorotwell@gmail.com>
2012-05-15 11:26:01 -0500

    added tests.

--- tests/FilesystemTest.php
+++ tests/FilesystemTest.php
@@ -9,6 +9,16 @@ class FilesystemTest extends PHPUnit_Framework_TestCase {
 		file_put_contents(__DIR__.'/file.txt', 'Hello World');
 		$files = new Filesystem;
 		$this->assertEquals('Hello World', $files->get(__DIR__.'/file.txt'));
+		@unlink(__DIR__.'/file.txt');
+	}
+
+
+	public function testPutStoresFiles()
+	{
+		$files = new Filesystem;
+		$files->put(__DIR__.'/file.txt', 'Hello World');
+		$this->assertEquals('Hello World', file_get_contents(__DIR__.'/file.txt'));
+		@unlink(__DIR__.'/file.txt');
 	}
 
 }
