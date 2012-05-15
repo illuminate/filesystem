4e4543462931f73e6b122b7cc26403d2e112541b
Taylor Otwell <taylorotwell@gmail.com>
2012-05-15 11:45:43 -0500

    added delete and clean directory.

--- src/Illuminate/Filesystem.php
+++ src/Illuminate/Filesystem.php
@@ -5,6 +5,17 @@ class FileNotFoundException extends \Exception {}
 class Filesystem {
 
 	/**
+	 * Determine if a file exists.
+	 *
+	 * @param  string  $path
+	 * @return bool
+	 */
+	public function exists($path)
+	{
+		return file_exists($path);
+	}
+
+	/**
 	 * Get the contents of a file.
 	 *
 	 * @param  string  $path
@@ -30,6 +41,18 @@ class Filesystem {
 	}
 
 	/**
+	 * Append to a file.
+	 *
+	 * @param  string  $path
+	 * @param  string  $data
+	 * @return int
+	 */
+	public function append($path, $data)
+	{
+		return file_put_contents($path, $data, LOCK_EX | FILE_APPEND);
+	}
+
+	/**
 	 * Delete the file at a given path.
 	 *
 	 * @param  string  $path
@@ -38,6 +61,129 @@ class Filesystem {
 	public function delete($path)
 	{
 		return unlink($path);
+	}
+
+	/**
+	 * Move a file to a new location.
+	 *
+	 * @param  string  $path
+	 * @param  string  $target
+	 * @return void
+	 */
+	public function move($path, $target)
+	{
+		return rename($path, $target);
+	}
+
+	/**
+	 * Copy a file to a new location.
+	 *
+	 * @param  string  $path
+	 * @param  string  $target
+	 * @return void
+	 */
+	public function copy($path, $target)
+	{
+		return copy($path, $target);
+	}
+
+	/**
+	 * Extract the file extension from a file path.
+	 * 
+	 * @param  string  $path
+	 * @return string
+	 */
+	public function extension($path)
+	{
+		return pathinfo($path, PATHINFO_EXTENSION);
+	}
+
+	/**
+	 * Get the file type of a given file.
+	 *
+	 * @param  string  $path
+	 * @return string
+	 */
+	public function type($path)
+	{
+		return filetype($path);
+	}
+
+	/**
+	 * Get the file size of a given file.
+	 *
+	 * @param  string  $path
+	 * @return int
+	 */
+	public function size($path)
+	{
+		return filesize($path);
+	}
+
+	/**
+	 * Get the file's last modification time.
+	 *
+	 * @param  string  $path
+	 * @return int
+	 */
+	public function modified($path)
+	{
+		return filemtime($path);
+	}
+
+	/**
+	 * Determine if the given path is a directory.
+	 *
+	 * @param  string  $directory
+	 * @return bool
+	 */
+	public function isDirectory($directory)
+	{
+		return is_dir($directory);
+	}
+
+	/**
+	 * Recursively delete a directory.
+	 *
+	 * The directory itself may be optionally preserved.
+	 *
+	 * @param  string  $directory
+	 * @param  bool    $preserve
+	 * @return void
+	 */
+	public function deleteDirectory($directory, $preserve = false)
+	{
+		if ( ! $this->isDirectory($directory)) return;
+
+		$items = new \FilesystemIterator($directory);
+
+		foreach ($items as $item)
+		{
+			// If the item is a directory, we can just recurse into the function and
+			// delete that sub-director, otherwise we'll just delete the file and
+			// keep iterating through each file until the directory is cleaned.
+			if ($item->isDir())
+			{
+				$this->deleteDirectory($item->getRealPath());
+			}
+			else
+			{
+				$this->delete($item->getRealPath());
+			}
+		}
+
+		if ( ! $preserve) @rmdir($directory);
+	}
+
+	/**
+	 * Empty the specified directory of all files and folders.
+	 *
+	 * @param  string  $directory
+	 * @return void
+	 */
+	public function cleanDirectory($directory)
+	{
+		return $this->deleteDirectory($directory, true);
 	}
 
 }
--- tests/FilesystemTest.php
+++ tests/FilesystemTest.php
@@ -31,4 +31,27 @@ class FilesystemTest extends PHPUnit_Framework_TestCase {
 		@unlink(__DIR__.'/file.txt');
 	}
 
+
+	public function testDeleteDirectory()
+	{
+		mkdir(__DIR__.'/foo');
+		file_put_contents(__DIR__.'/foo/file.txt', 'Hello World');
+		$files = new Filesystem;
+		$files->deleteDirectory(__DIR__.'/foo');
+		$this->assertFalse(is_dir(__DIR__.'/foo'));
+		$this->assertFalse(file_exists(__DIR__.'/foo/file.txt'));
+	}
+
+
+	public function testCleanDirectory()
+	{
+		mkdir(__DIR__.'/foo');
+		file_put_contents(__DIR__.'/foo/file.txt', 'Hello World');
+		$files = new Filesystem;
+		$files->cleanDirectory(__DIR__.'/foo');
+		$this->assertTrue(is_dir(__DIR__.'/foo'));
+		$this->assertFalse(file_exists(__DIR__.'/foo/file.txt'));
+		@rmdir(__DIR__.'/foo');
+	}
+
 }
