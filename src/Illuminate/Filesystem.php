8736f8981ac5f4e034ef0645588ca813a00b7025
Taylor Otwell <taylorotwell@gmail.com>
2012-10-27 17:32:42 -0500

    initial pass at copyDirectory function.

--- src/Illuminate/Filesystem.php
+++ src/Illuminate/Filesystem.php
@@ -1,5 +1,7 @@
 <?php namespace Illuminate;
 
+use FilesystemIterator;
+
 class FileNotFoundException extends \Exception {}
 
 class Filesystem {
@@ -200,6 +202,69 @@ class Filesystem {
 	}
 
 	/**
+	 * Create a directory.
+	 *
+	 * @param  string  $path
+	 * @param  int     $mode
+	 * @param  bool    $recursive
+	 * @return bool
+	 */
+	public function makeDirectory($path, $mode = 0777, $recursive = false)
+	{
+		return mkdir($path, $mode, $recursive);
+	}
+
+	/**
+	 * Copy a directory from one location to another.
+	 *
+	 * @param  string  $directory
+	 * @param  string  $destination
+	 * @param  int     $options
+	 * @return void
+	 */
+	public function copyDirectory($directory, $destination, $options = null)
+	{
+		if ( ! $this->isDirectory($directory)) return false;
+
+		$options = $options ?: FilesystemIterator::SKIP_DOTS;
+
+		// If the destination directory does not actually exist, we will go ahead and
+		// create it recursively, which just gets the destination prepared to copy
+		// the files over. Once we make the directory we'll proceed the copying.
+		if ( ! $this->isDirectory($destination))
+		{
+			$this->makeDirectory($destination, 0777, true);
+		}
+
+		$items = new FilesystemIterator($directory, $options);
+
+		foreach ($items as $item)
+		{
+			// As we spin through items, we will check to see if the current file is actually
+			// a directory or a file. When it is actually a directory we will need to call
+			// back into this function recursively to keep copying these nested folders.
+			$target = $destination.'/'.$item->getBasename();
+
+			if ($item->isDir())
+			{
+				$path = $item->getRealPath();
+
+				if ( ! $this->copyDirectory($path, $target, $options)) return false;
+			}
+
+			// If the current items is just a regular file, we will just copy this to the new
+			// location and keep looping. If for some reason the copy fails we'll bail out
+			// and return false, so the developer is aware that the copy process failed.
+			else
+			{
+				if ( ! $this->copy($item->getRealPath(), $target)) return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
 	 * Recursively delete a directory.
 	 *
 	 * The directory itself may be optionally preserved.
@@ -223,6 +288,10 @@ class Filesystem {
 			{
 				$this->deleteDirectory($item->getRealPath());
 			}
+
+			// If the item is just a file, we can go ahead and delete it since we're
+			// just looping through and waxing all of the files in this directory
+			// and calling directories recursively, so we delete the real path.
 			else
 			{
 				$this->delete($item->getRealPath());
